3bd4ac5d891649aab812aa6509a1e6b6a0e0c2b5
klequis <klequis@gmail.com>
2019-01-24 21:47:04 -0800

    init

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import React from 'react'
+import injectSheet from 'react-jss'
+import { Button} from '@material-ui/core'
+import FabInk from './FabInk'
+import TextButtonCss from './TextButtonCss'
+import TextButtonInk from './TextButtonInk'
+import Fab from '@material-ui/core/Fab';
+
+
+const Td = ({ children }) => {
+  return <td style={{ padding: 10, fontSize: '0.9em' }}>{children}</td>
+}
+
+const App = ({ classes }) => {
+
+  return (
+    <div className={classes.wrapper}>
+      <h1>Replicating Material Design/UI Buttons.</h1>
+      <p>The below are buttons which imitate the look of Material Design buttons, including their characteristic ripple effect.</p>
+      <div className={classes.textButtons}>
+        <h2 className={classes.h2}>Text Button</h2>
+        <p>The MUI-LIKE CSS button is very much like both of the others but notice that the center of its ripple effect is always in the middle of the button regardless of where it is clicked. With the actual MUI button and the imitation using <code>react-ink</code>, the center of the ripple is where the user clicks.</p>
+        <table>
+          <tbody>
+            <tr>
+              <Td>
+                MUI Text Button
+              </Td>
+              <Td>
+                <Button color="primary" className={classes.muiButton}>
+                  MUI Primary
+                </Button>
+              </Td>
+            </tr>
+            <tr>
+              <Td>
+                MUI Imatation using CSS
+              </Td>
+              <td>
+                <TextButtonCss>MUI-like CSS</TextButtonCss>
+              </td>
+            </tr>
+            <tr>
+              <Td>
+                MUI Imatation using <code>react-ink</code>
+              </Td>
+              <Td>
+                <TextButtonInk>MUI-like Ink</TextButtonInk>
+              </Td>
+            </tr>
+
+          </tbody>
+        </table>
+      </div>
+      <div className={classes.fabButtons}>
+        <h2 className={classes.h2}>Floating Action Button (FAB)</h2>
+
+        <table>
+          <tbody>
+            <tr>
+              <Td>
+                MUI FAB Button
+              </Td>
+              <Td>
+              <Fab size="small" color="secondary">
+                H
+              </Fab>
+              </Td>
+            </tr>
+            <tr>
+
+              <Td>
+                MUI Immatation using <code>react-ink</code>
+              </Td>
+              <Td>
+                <FabInk/>
+              </Td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+
+const styles = {
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    minHeight: '100vh',
+    color: 'white',
+    backgroundColor: '#282c34',
+    width: '100%',
+  },
+  muiButton: {
+    color: 'rgb(33, 150, 243)'
+  },
+  row: {
+    display: 'flex',
+    fontSize: '.5em',
+    backgroundColor: 'green',
+    width: '100%',
+  },
+  textButtons: {
+    width: '50%',
+  },
+  fabButtons: {
+    width: '50%',
+  },
+  h2: {
+    fontSize: '2rem',
+  }
+}
+
+export default injectSheet(styles)(App)
---
+++ src/TextButtonCss.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import injectSheet from 'react-jss'
+import classNames from 'classnames'
+
+const TextButton = ({ children, classes }) => {
+  const clsNames =  classNames([
+    classes.button,
+    classes.ripple,
+  ])
+  return <button className={clsNames}>{children}</button>
+}
+
+const backgroundColor = 'rgba(0, 0, 0, 0)'
+const primaryColor = 'rgb(33, 150, 243)'
+
+const styles = {
+  button: {
+    alignItems: 'center',
+    backgroundColor: backgroundColor,
+    borderRadius: 4,
+    borderStyle: 'none',
+    borderWidth: 0,
+    borderColor: primaryColor,
+    borderImageWidth: 1,
+    borderImageOutset: 0,
+    borderImageRepeat: 'stretch',
+    borderImageSlice: '100%',
+    borderImageSource: 'none',
+    boxSizing: 'border-box',
+    color: primaryColor,
+    cursor: 'pointer',
+    display: 'inline-flex',
+    fontFamily: "'Roboto', sans-serif;sans-serif",
+    fontSize: 14,
+    fontWeight: 500,
+    justifyContent: 'center',
+    letterSpacing: 'normal',
+    lineHeight: '24.5px',
+    margin: 0,
+    minWidth: 64,
+    outlineColor: primaryColor,
+    outlineStyle: 'none',
+    outlineWidth: 0,
+    padding: '6px 8px',
+    position: 'relative',
+    textDecoration: 'none',
+    textTransform: 'uppercase',
+    transitionDelay: '0s, 0s, 0s',
+    transitionDuration: '0.25s, 0.25s, 0.25s',
+    transitionProperty: 'background-color, box-shadow, border',
+    transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1), cubic-bezier(0.4, 0, 0.2, 1), cubic-bezier(0.4, 0, 0.2, 1)',
+    userSelect: 'none',
+    verticalAlign: 'middle',
+    MozAppearance: 'none',
+    MozUserSelect: 'none',
+    WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)',
+    '&:hover': {
+      backgroundColor: 'rgba(63, 81, 181, 0.08)',
+      textDecoration: 'none',
+    },
+  },
+  ripple: {
+    transition: 'background 0.8s',
+    '&:hover': {
+      background: `transparent radial-gradient(circle, transparent 1%, rgba(33, 150, 243, .1) 1%) center/15000%`, //++
+    },
+    '&:active': {
+      backgroundColor: 'transparent', //+
+      backgroundSize: '100%',
+      transition: 'background 0s',
+    },
+  }
+}
+export default injectSheet(styles)(TextButton)
